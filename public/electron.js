102fed3af6d2ea1e7ea13008c876d811b986628c
Liwen Guo <lg@livven.me>
2020-02-16 16:57:15 +0100

    Add React dev tools

--- public/electron.js
+++ public/electron.js
@@ -10,9 +10,18 @@ function createWindow() {
   if (process.env.NODE_ENV === "development") {
     window.loadURL("http://localhost:3000");
     window.webContents.openDevTools();
+    addDevTools();
   } else {
     window.loadFile("build/index.html");
   }
+}
+
+async function addDevTools() {
+  const {
+    default: installExtension,
+    REACT_DEVELOPER_TOOLS,
+  } = require("electron-devtools-installer");
+  await installExtension(REACT_DEVELOPER_TOOLS);
 }
 
 app.whenReady().then(createWindow);
